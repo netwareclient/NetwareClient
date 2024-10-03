d9dca169c4b2756852179c0c4b37c3d2a93b71ad
Sudarshan Mhasrup <sudarshanmhasrup@outlook.com>
2024-10-03 17:34:50 +0530

    Define library source directory as /library/src/main

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,9 +1,17 @@
 plugins {
-
+    alias(libs.plugins.kotlin)
 }
 
 repositories {
+    mavenCentral()
+}
 
+
+// Custom source code directory
+sourceSets {
+    main {
+        kotlin.srcDirs("src/main")
+    }
 }
 
 dependencies {
