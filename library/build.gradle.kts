3efd64151c148eaf89a4dddf530bcffaa789ba39
Sudarshan Mhasrup <sudarshanmhasrup@outlook.com>
2024-10-04 09:10:03 +0530

    Add java source compatibility for VERSION_11

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -18,6 +18,11 @@ dependencies {
 
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
 // Custom source code directory
 sourceSets {
     main {
