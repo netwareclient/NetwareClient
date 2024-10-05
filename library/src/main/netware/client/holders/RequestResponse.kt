547f2d9d926ec9a4d63c5b33cdd369fd57ed8362
Sudarshan Mhasrup <sudarshanmhasrup@outlook.com>
2024-10-05 13:41:48 +0530

    Add a function in RequestResponse data class to access headers

--- library/src/main/netware/client/holders/RequestResponse.kt
+++ library/src/main/netware/client/holders/RequestResponse.kt
@@ -4,11 +4,13 @@ data class RequestResponse(
     private val status: String = "No status found.",
     private val statusCode: Int = 0,
     private val response: String = "No response found.",
+    private val headers: Map<String, String> = mapOf()
 ) {
 
     fun getStatus() = status
     fun getStatusCode() = statusCode
     fun getResponse() = response
+    fun getHeaders() = headers
     fun getLog() {
 
     }
