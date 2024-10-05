61770e45825166463bd7f3d2c54c13ffe3ecf341
Sudarshan Mhasrup <sudarshanmhasrup@outlook.com>
2024-10-05 18:22:46 +0530

    Create test to validation error message on invalid network request protocol

--- library/src/test/netware/client/executors/RequestClientExecutorTest.kt
+++ library/src/test/netware/client/executors/RequestClientExecutorTest.kt
@@ -6,5 +6,17 @@ import kotlin.test.assertEquals
 
 class RequestClientExecutorTest {
 
-     
+    @Test
+    fun testRequestClientValidation() {
+
+        val requestClientExecutor = RequestClientExecutor(
+            networkRequestUrl = "httxp://127.0.0.1:8080/",
+            networkRequestMethod = "POST"
+        ).validateNetworkRequest()
+
+        assertEquals("""
+            Status code: 1000, Status: Failed.
+            Message: Invalid network request protocol.
+        """.trimIndent(), requestClientExecutor.getError().getErrorLog())
+    }
 }
