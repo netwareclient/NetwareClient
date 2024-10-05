cf7f86352c5bc52a05a2d799222b30a92d74ff19
Sudarshan Mhasrup <sudarshanmhasrup@outlook.com>
2024-10-05 13:48:30 +0530

    Create RequestError data class for network request

---
+++ library/src/main/netware/client/holders/RequestError.kt
@@ -0,0 +1,15 @@
+package netware.client.holders
+
+data class RequestError(
+    private val status: String = "No error status found.",
+    private val statusCode: Int = 0,
+    private val message: String = "No error message found.",
+) {
+
+    fun getStatusCode() = statusCode
+    fun getStatus() = status
+    fun getMessage() = message
+    fun getErrorLog() {
+
+    }
+}
