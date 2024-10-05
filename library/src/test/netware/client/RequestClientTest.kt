948db6f62b4bec890cd46dbca2c2ad37883e1ea2
Sudarshan Mhasrup <sudarshanmhasrup@outlook.com>
2024-10-05 16:30:51 +0530

    Create test to check invalid network reques method formatted log

--- library/src/test/netware/client/RequestClientTest.kt
+++ library/src/test/netware/client/RequestClientTest.kt
@@ -55,5 +55,28 @@ class RequestClientTest {
         assertEquals(true, requestClient.isNetworkRequestMethodValid())
     }
 
+    @Test
+    fun checkInvalidNetworkRequestErrorLogFormatted() {
 
+        val requestClient = RequestClient(
+            url = "http://127.0.0.1:8080/",
+            method = "UMM"
+        ).build()
+
+        if (requestClient.isSuccess) {
+
+        } else {
+            assertEquals("""
+                
+                --------------------------------------------------
+                Status code: 1000, Status: Failed.
+                Message: -----------------------------------------
+                "UMM" is not a valid HTTP request method.
+                --------------------------------------------------
+                
+            """.trimIndent(), requestClient.error.getErrorLog(
+                formated = true
+            ))
+        }
+    }
 }
