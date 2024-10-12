546973fe51a6accb9900f26470d66e013370ff68
Sudarshan Mhasrup <sudarshanmhasrup@outlook.com>
2024-10-12 09:19:26 +0530

    Resolve Kotlin JVM issue

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    id("org.jetbrains.kotlin.jvm")
     alias(libs.plugins.springBoot)
 }
 
--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    alias(libs.plugins.kotlin)
     alias(libs.plugins.mavenPublish)
 }
 
