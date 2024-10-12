2bcd1276738d782f09a274c7e68f787b5055c6fb
Sudarshan Mhasrup <sudarshanmhasrup@outlook.com>
2024-10-12 09:22:42 +0530

    Remove plugin management

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,5 @@
 rootProject.name = "NetwareClient"
 
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        mavenCentral()
-    }
-
-    plugins {
-        id("org.jetbrains.kotlin.jvm") version "2.0.20"
-    }
-}
-
 include(":library")
 include(":api")
 include(":snippets")
